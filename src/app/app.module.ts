98cb9bf0d8344ff4524b8af282ea78ec9c8e3b4e
BrianHCombes <briancombes59@yahoo.com>
2018-09-25 22:09:47 -0600

    component building and experimenting. Latest is to add the TinyMCE editor to the app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { EditorModule } from '@tinymce/tinymce-angular';
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question/question.component';
+import { FirebaseService   } from './firebase.service';
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent
   ],
+  
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    EditorModule
   ],
-  providers: [],
+  providers: [FirebaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
